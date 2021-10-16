d34559911ee33e831daf2733e3073c457f560187
George <228lol322kek@gmail.cpm>
2021-10-16 03:41:10 +0300

    fixes

--- src/components/ProductGrid/ProductGrid.tsx
+++ src/components/ProductGrid/ProductGrid.tsx
@@ -4,62 +4,27 @@ import { ProductBox } from '../ProductBox';
 import styles from './productgrid.module.scss';
 
 export function ProductGrid() {
-  const [products, setProducts] = useState<any[]>([]);
-  const [loading, setLoading] = useState(false);
-  const [errorLoading, setErrorLoading] = useState('');
+
   const bottomOfList = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    async function load() {
-      setLoading(true);
-      setErrorLoading('');
-  
-      try {
-        const response = await fetch('https://fakestoreapi.com/products/?limit=20');
-        const result = await response.json();
-
-        //const result = await axios.get('https://fakestoreapi.com/products?limit=20');
-        setProducts(prev => prev.concat(...result));
-        console.log(result);
-
-      } catch (error) {
-        setErrorLoading(String(error));
-      }
-    }
-
-    load();
-
-  //   const observer = new IntersectionObserver((entries) => {
-  //     if (entries[0].isIntersecting) {
-  //       load()
-  //     }
-  //   }, {
-  //     rootMargin: '100px',
-  //   })
-    
-  //   if(bottomOfList.current) {
-  //     observer.observe(bottomOfList.current)
-  //   }
-
-  //   return () => {
-  //     if(bottomOfList.current) {
-  //       observer.unobserve(bottomOfList.current)
-  //     }
-  //   }
-
-  }, [bottomOfList])
+  const tempArr = [...Array(10)];
+  const staticInfo = {
+    img: '',
+    name: 'Tiny Potato Face',
+    desc: 'Classic Tee',
+    price: '$20',
+  }
 
   return (
     
     <div className={styles.cont}>
 
-      {products.map(product => (
+      {tempArr.map(ind => (
         <ProductBox
-          key={product.id}
-          img={product.image}
-          name={product.title}
-          desc='blank for now'
-          price={product.price}
+            key={ind}
+            img={staticInfo.img}
+            name={staticInfo.name}
+            desc={staticInfo.desc}
+            price={staticInfo.price}
         />
       ))}
 
