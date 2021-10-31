8f876c780fcd7761bd0eb47a74220d065d0993b4
twep <228lol322kek@gmail.com>
2021-10-31 19:25:51 +0300

    update

--- src/components/CartItem/CartItem.tsx
+++ src/components/CartItem/CartItem.tsx
@@ -7,9 +7,15 @@ export function CartItem({ name, desc, img, price, amount, size }: ICartProduct)
   return(
     <div className={styles.container}>
       <div className={styles.productBlock}>
-        <img className={styles.preview} src="" alt="" />
+        <img className={styles.preview} src={img} alt="" />
         <div className={styles.infGrid}>
-
+          <div className={styles.title}>{name}</div>
+          <div className={styles.items}>
+            <div style={{fontSize: '1.6rem', margin: '1rem 0'}}>{desc}</div>
+            <div>Size - {size}</div>
+            <div>Color - Pink</div>
+          </div>
+          <a className={styles.remove}>Remove</a>
         </div>
       </div>
     </div>
