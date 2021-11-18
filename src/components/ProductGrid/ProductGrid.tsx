0d290baa7e7daa60fb52cde06abb0ae12041d46f
twep <228lol322kek@gmail.com>
2021-11-18 22:07:21 +0300

    feat: going insane

--- src/components/ProductGrid/ProductGrid.tsx
+++ src/components/ProductGrid/ProductGrid.tsx
@@ -13,14 +13,64 @@ interface IProps {
 }
 
 export function ProductGrid({data}: IProps) {
+  const [products, setProducts] = useState<any[]>([...Array(8)]);
+  const [loading, setLoading] = useState(false)
 
   const bottomOfList = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    async function load() {
+      setLoading(true)
+      
+      try {
+        const answer = await new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve([...Array(8)])
+          }, 1000)
+        })
+        setProducts(prevChild => prevChild.concat(answer))
+
+      } catch (e) {
+        console.log(String(e))
+      }
+
+      setLoading(false)
+    }
+    
+    const observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        load()
+      }
+    }, {
+      // родитель целевого элемента - область просмотра
+      root: null,
+      // без отступов
+      rootMargin: '0px',
+      // процент пересечения - половина изображения
+      threshold: 0.5
+    })
+    
+    if(bottomOfList.current) {
+      observer.observe(bottomOfList.current)
+    }
+
+    return () => {
+      if(bottomOfList.current) {
+        observer.unobserve(bottomOfList.current)
+      }
+    }
+
+
+  }, [bottomOfList])
 
   return (
     
     <div className={styles.cont}>
+      {products.length === 0 && !loading && (
+        <div className={styles.loading}>Нет ни одного поста</div>
+      )}
 
-      {[...Array(10)].map((_, index) => (
+      {products.map((_, index) => (
         <ProductBox
             key={index}
             img={data.img}
@@ -31,7 +81,9 @@ export function ProductGrid({data}: IProps) {
       ))}
 
       <div ref={bottomOfList}></div>
-
+      {loading && (
+        <div className={styles.loading}>Загрузка...</div>
+      )}
     </div>
   );
 }
