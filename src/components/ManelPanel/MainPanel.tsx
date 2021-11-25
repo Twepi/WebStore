fa2d085516f0df3ce9a61d4fad4c4520e9ffdfc7
twep <228lol322kek@gmail.com>
2021-11-25 13:29:48 +0300

    fixes

--- src/components/ManelPanel/MainPanel.tsx
+++ src/components/ManelPanel/MainPanel.tsx
@@ -1,32 +1,37 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { FlyoutMenu } from '../FlyoutMenu';
-import { MenuActions } from '../MenuActions';
-import { MenuItem } from '../MenuItem/MenuItem';
-import styles from './mainpanel.module.scss';
+import React from "react";
+import { Link } from "react-router-dom";
+import { FlyoutMenu } from "../FlyoutMenu";
+import { MenuActions } from "../MenuActions";
+import { MenuItem } from "../MenuItem/MenuItem";
+import styles from "./mainpanel.module.scss";
 
 export function MainPanel() {
-
   return (
     <div className={styles.panel}>
       <div className={styles.container}>
-        <Link to='/'>
-          <img className={styles.logo} src="https://www.mylogohouse.com/category/fashion/img-lotus-flower-logo-design-01/lotus-flower-logo-design-800x500-color.jpg" alt="Logo" />
+        <Link to="/">
+          <span>Home</span>
         </Link>
 
-      <div className={styles.menucontainer}>
-        <MenuItem name='Apparel'>
-          <FlyoutMenu items={["unisex / men's t-shirts", "women's t-shirts", "hoodies & sweatshirts"]}/>
-        </MenuItem>
+        <div className={styles.menucontainer}>
+          <MenuItem name="Apparel">
+            <FlyoutMenu
+              items={[
+                "unisex / men's t-shirts",
+                "women's t-shirts",
+                "hoodies & sweatshirts",
+              ]}
+            />
+          </MenuItem>
 
-        <MenuItem name='Homeware'>
-          <FlyoutMenu items={["pillows", "bath", "bedding"]}/>
-        </MenuItem>
+          <MenuItem name="Homeware">
+            <FlyoutMenu items={["pillows", "bath", "bedding"]} />
+          </MenuItem>
 
-        <MenuItem name='Accessories'>
-          <FlyoutMenu items={["face masks", "phone cases", "socks"]}/>
-        </MenuItem>
-      </div>
+          <MenuItem name="Accessories">
+            <FlyoutMenu items={["face masks", "phone cases", "socks"]} />
+          </MenuItem>
+        </div>
 
         <MenuActions />
       </div>
